2dc9d41d64d872384e060791e8847f88c5ba6d0c
jingquan-wang <jq.wang1214@gmail.com>
2020-12-26 22:59:14 -0500

    sql easy 627 swap salary

---
+++ SQL/easy_627_swap_salary.sql
@@ -0,0 +1,34 @@
+-- Given a table salary, such as the one below, that has m=male and f=female values. Swap all f and m values (i.e., change all f values to m and vice versa) with a single update statement and no intermediate temp table.
+
+-- Note that you must write a single update statement, DO NOT write any select statement for this problem.
+
+ 
+
+-- Example:
+
+-- | id | name | sex | salary |
+-- |----|------|-----|--------|
+-- | 1  | A    | m   | 2500   |
+-- | 2  | B    | f   | 1500   |
+-- | 3  | C    | m   | 5500   |
+-- | 4  | D    | f   | 500    |
+-- After running your update statement, the above salary table should have the following rows:
+-- | id | name | sex | salary |
+-- |----|------|-----|--------|
+-- | 1  | A    | f   | 2500   |
+-- | 2  | B    | m   | 1500   |
+-- | 3  | C    | f   | 5500   |
+-- | 4  | D    | m   | 500    |
+
+-- Solution 1 (mysql): use IF(condition, if_true, if_false) 
+UPDATE salary
+SET 
+    sex = IF(sex="m", "f", "m")
+    
+-- Solution 2: use CASE ... WHEN ... THEN ... ELSE ... END
+UPDATE salary
+SET 
+    sex = CASE sex
+        WHEN "f" THEN "m"
+        ELSE "f"
+    END;
